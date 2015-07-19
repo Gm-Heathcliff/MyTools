ec0d2d8e0f643b28fb71762006c76d8ff621f5be
Gm-Heathcliff <Gm-Heathcliff@web.de>
2015-07-19 22:20:40 +0100

    Update MyTexturesGeneratorGround.bat

--- MyTexturesGeneratorGround.bat
+++ MyTexturesGeneratorGround.bat
@@ -1,1 +1,1 @@
-FOR /f %%i IN (bmp.txt) DO echo custom/MyTextures/MyGrounds/%%i"|MyTextures/MyGrounds/%%i" >> MyGroundsTextures.txt
+FOR /f %%i IN (bmp.txt) DO echo MyTextures/MyGrounds/%%i"|MyTextures\MyGrounds\%%i" >> MyGroundsTextures.txt
