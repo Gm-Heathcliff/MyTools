1919baa3c3820042b9d899b23810d7867a5156ca
Gm-Heathcliff <Gm-Heathcliff@web.de>
2015-07-19 16:04:53 +0100

    Update and rename MyTexturesGenerator.bat to MyTexturesGeneratorGround.bat

--- MyTexturesGenerator.bat
+++
@@ -1,1 +0,0 @@
-FOR /f %%i IN (bmp.txt) DO echo custom/MyTextures/%%i"|MyTextures/%%i" >> MyTextures.txt
---
+++ MyTexturesGeneratorGround.bat
@@ -0,0 +1,1 @@
+FOR /f %%i IN (bmp.txt) DO echo custom/MyTextures/MyGrounds/%%i"|MyTextures/MyGrounds/%%i" >> MyGroundsTextures.txt
